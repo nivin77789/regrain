13a086b42649900bdc35d7b8364a79dbc2378727
Nivin Mathew S <nivinmathews@Nivins-MacBook-Air.local>
2025-12-02 15:53:39 +0530

    gaesdg

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -11,7 +11,7 @@ import { Footer } from "@/components/Footer";
 
 const Index = () => {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen animated-bg">
       <Navbar />
       <Hero />
       <About />
